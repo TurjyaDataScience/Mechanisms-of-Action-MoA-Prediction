49923b987ff2f8b0c6ba23addc20402b1f7538be
“Turjya <turjyadatascience@gmail.com>
2024-07-29 01:44:28 +0530

    1st Script R

---
+++ figures/Explanatory_Data_Analysis.R
@@ -0,0 +1,12 @@
+# Library
+
+library(tidyverse)
+
+# dataset loading
+
+train_features <- read_csv("C:/Users/Lenovo/Desktop/Mechanisms-of-Action-MoA-Prediction/data/train_features.csv")
+train_drug <- read_csv("C:/Users/Lenovo/Desktop/Mechanisms-of-Action-MoA-Prediction/data/train_drug.csv")
+train_targets_nonscored<-read_csv("C:/Users/Lenovo/Desktop/Mechanisms-of-Action-MoA-Prediction/data/train_targets_nonscored.csv")
+train_tarets_scored<- read_csv("C:/Users/Lenovo/Desktop/Mechanisms-of-Action-MoA-Prediction/data/train_targets_scored.csv")
+
+
